57931c8a75a29f05f1ed835da125c64ac1eab547
junjieProjects <zljunjie@163.com>
2014-10-20 17:48:27 +0800

    mofify

--- Game_ios/LingYangViewController.m
+++ Game_ios/LingYangViewController.m
@@ -27,7 +27,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        NSLog(@"==========");
     }
     return self;
 }
