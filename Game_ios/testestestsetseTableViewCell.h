6aa9ecf4c52697608a5c2fcde369c82b515bc0b4
张俊杰 <zhangjunjie@letoke.com>
2014-10-20 14:25:11 +0800

    my second commit

---
+++ Game_ios/testestestsetseTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  testestestsetseTableViewCell.h
+//  Game_ios
+//
+//  Created by Paul on 14-10-20.
+//  Copyright (c) 2014年 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testestestsetseTableViewCell : UITableViewCell
+
+@end
---
+++ Game_ios/testestestsetseTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  testestestsetseTableViewCell.m
+//  Game_ios
+//
+//  Created by Paul on 14-10-20.
+//  Copyright (c) 2014年 Paul. All rights reserved.
+//
+
+#import "testestestsetseTableViewCell.h"
+
+@implementation testestestsetseTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
